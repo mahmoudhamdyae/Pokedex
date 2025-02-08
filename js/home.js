fcf53156991a18ffb87014afa125e6a5b2abab63
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2025-02-08 17:12:29 +0200

    nav bar desktop finish

--- js/home.js
+++ js/home.js
@@ -1,14 +1,18 @@
-const burgerIcon = document.querySelector(".nav-bar-items .burger");
+const burgerIcon = document.querySelector(".burger");
+const navBar = document.querySelector(".nav-bar");
+const navBarItems = document.querySelector(".nav-bar-items");
 
-function openNavBar() {
-    var x = document.getElementById("nav-bar-items");
-    if (x.className === "nav-bar-item") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav-bar-item";
+navBarItems.style.maxHeight = "0px";
+
+function toggleNavBar() {
+    if (getComputedStyle(navBar).height == "115px") {
+        navBar.style.height = "344px";
+        navBar.style.background = "linear-gradient(180deg, #F5DB13 0%, #F2B807 100%)";
+        navBar.style.borderRadius = "0px 0px 16px 16px";
+        burgerIcon.style.display = "none";
     }
   }
 
   burgerIcon.addEventListener("click", () => {
-    openNavBar();
+    toggleNavBar();
 });
