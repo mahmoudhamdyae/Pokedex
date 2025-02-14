67c1bcfeee13dddca555eb889b780d92e9f51560
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2025-02-14 16:54:54 +0200

    Finish Nav Bar Design

--- js/home.js
+++ js/home.js
@@ -1,18 +1,19 @@
 const burgerIcon = document.querySelector(".burger");
 const navBar = document.querySelector(".nav-bar");
-const navBarItems = document.querySelector(".nav-bar-items");
+const overlay = document.querySelector(".overlay");
 
-navBarItems.style.maxHeight = "0px";
+function toggleNavbar(event) {
+  navBar.classList.toggle("nav-bar-selected");
+  overlay.classList.toggle("active");
+}
 
-function toggleNavBar() {
-    if (getComputedStyle(navBar).height == "115px") {
-        navBar.style.height = "344px";
-        navBar.style.background = "linear-gradient(180deg, #F5DB13 0%, #F2B807 100%)";
-        navBar.style.borderRadius = "0px 0px 16px 16px";
-        burgerIcon.style.display = "none";
+  function closeNavbar(event) {
+    if (!navBar.contains(event.target)) {
+      navBar.classList.remove("nav-bar-selected");
+      overlay.classList.remove("active");
     }
-  }
+}
 
-  burgerIcon.addEventListener("click", () => {
-    toggleNavBar();
-});
+burgerIcon.addEventListener("click", toggleNavbar);
+overlay.addEventListener("click", closeNavbar);
+document.addEventListener("click", closeNavbar);
